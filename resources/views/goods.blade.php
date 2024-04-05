e2caa0a9f4fdeb09a43bd549306f4511b5e209df
rono516 <ronocollins2000@gmail.com>
2024-04-05 16:10:32 +0300

    feature/no-ref/add-order-page

--- resources/views/goods.blade.php
+++ resources/views/goods.blade.php
@@ -5,24 +5,26 @@
         <div class="container">
 
             <div class="row">
-                @foreach ($products as $product )
-                <div class="col-lg-4 col-md-6 col-12">
-                    <!-- Single Blog -->
-                    <div class="single-news">
-                        <div class="news-head">
-                            <img src="{{ Storage::url($product->image_url) }}" alt="#">
-                        </div>
-                        <div class="news-body">
-                            <div class="news-content">
-                                {{-- <div class="date"><button class="date">22 Aug, 2020</button></div> --}}
-                                <h2><a href="{{ route('products.show', $product) }}">{{ $product->name }}</a></h2>
-                                <p>{{ $product->description }}</p>
-                                <h3 class="text">Ksh. {{ $product->price }}</h3>
+                @foreach ($products as $product)
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <a href="{{ route('products.show', $product) }}">
+                            <!-- Single Blog -->
+                            <div class="single-news">
+                                <div class="news-head">
+                                    <img src="{{ Storage::url($product->image_url) }}" alt="#">
+                                </div>
+                                <div class="news-body">
+                                    <div class="news-content">
+                                        {{-- <div class="date"><button class="date">22 Aug, 2020</button></div> --}}
+                                        <h2>{{ $product->name }}</h2>
+                                        <p>{{ $product->description }}</p>
+                                        <h3 class="text">Ksh. {{ $product->price }}</h3>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </a>
+                        <!-- End Single Blog -->
                     </div>
-                    <!-- End Single Blog -->
-                </div>
                 @endforeach
 
 
--- resources/views/single_product.blade.php
+++ resources/views/single_product.blade.php
@@ -1,5 +1,58 @@
 @extends('frontendlayouts.main')
 
 @section('content')
-    <p class="text-center"> Here is the product detail page</p>
+    <!-- Start Why choose -->
+    <section class="why-choose section">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <!-- Start Choose Rights -->
+                    <div class="choose-right">
+                        <div class="video-image">
+                            <img src="{{ Storage::url($product->image_url) }}" alt="#">
+                            <!-- Video Animation -->
+                            {{-- <div class="promo-video">
+									<div class="waves-block">
+										<div class="waves wave-1"></div>
+										<div class="waves wave-2"></div>
+										<div class="waves wave-3"></div>
+									</div>
+								</div>
+								<!--/ End Video Animation -->
+								<a href="https://www.youtube.com/watch?v=RFVXy6CRVR4" class="video video-popup mfp-iframe"><i class="fa fa-play"></i></a> --}}
+                        </div>
+                    </div>
+                    <!-- End Choose Rights -->
+                </div>
+                <div class="col-lg-6 col-12">
+                    <!-- Start Choose Left -->
+                    <div class="choose-left">
+                        <h3>{{ $product->name }}</h3>
+                        <p>{{ $product->description }}</p>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list">
+                                    <li><i class="fa fa-minus"></i>Quantity: {{ $product->quantity }} </li>
+
+
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="list">
+                                    <li><i class="fa fa-earth-americas"></i>Location: {{ $product->location }}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="mt-5">
+                            <button class="btn ">Order Now</button>
+                        </div>
+                    </div>
+                    <!-- End Choose Left -->
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--/ End Why choose -->
 @endsection
